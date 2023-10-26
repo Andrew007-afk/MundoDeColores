518b7d56b1c52c8cec6ff249819ef12da4861723
Andrew007-afk <145103752+Andrew007-afk@users.noreply.github.com>
2023-10-26 11:11:52 +0200

    Add files via upload

---
+++ Mundo de colores/script.js
@@ -0,0 +1,19 @@
+function fRojo(){
+    document.querySelector("section").style.background = "#c24343"
+}
+
+function fAzul(){
+    document.querySelector("section").style.background = "rgb(69, 174, 206)"
+}
+
+function fVerde(){
+    document.querySelector("section").style.background = "#89c29a"
+}
+
+function fAmarillo(){
+    document.querySelector("section").style.background = "#becc3e"
+}
+
+function fReset(){
+    document.querySelector("section").style.background = "white"
+}
